4d39efcc15abbb8a139b1e1c3241c1c592addb11
Wolf Mermelstein <wolf@404wolf.com>
2025-02-01 20:08:52 -0500

    add file parser

---
+++ src/main/java/com/cwru/PasswordFileParser.java
@@ -0,0 +1,14 @@
+package com.cwru;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class PasswordFileParser {
+  public static Map<String, String> loadFile(String filename) {
+    Map<String, String> map = new HashMap<>();
+    return map;
+  }
+
+  public static void dumpFile(Map<String, String> map) {
+  }
+}
